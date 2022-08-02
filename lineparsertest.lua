914306ad8c5313637f0b4352790444306f0a3982
pd3v <paulo.develop@gmail.com>
2022-08-02 17:10:59 +0100

    code cleaning

--- lineparsertest.lua
+++ lineparsertest.lua
@@ -1,8 +1,8 @@
-local lpeg = require 'lpeg'
-lpeg.locale(lpeg) 
+-- local lpeg = require 'lpeg'
+-- lpeg.locale(lpeg) 
 
 math.randomseed(os.time())
 
 offset = 23
-a = lpeg
+
 a = offset+(math.random()*100)%(90-offset)
