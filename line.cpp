a5556b5f977a6f59cafeaa6816eb30f3f2edc83a
pd3v <paulo.develop@gmail.com>
2023-07-25 16:12:28 +0100

    echo report lua errors

--- line.cpp
+++ line.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <chrono>
 #include <future>
@@ -13,13 +14,11 @@
 #include <vector>
 #include <deque>
 #include <stdexcept>
-#include <sstream>
 #include <stdlib.h>
 #include <algorithm>
 #include <tuple>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <thread>
 #include "externals/link/examples/linkaudio/AudioPlatform_Dummy.hpp"
 #include "externals/rtmidi/RtMidi.h"
 
@@ -130,16 +129,24 @@ public:
   }
   ~Parser() {lua_close(L);};
   
+  void reportErrors(lua_State *L, int status) {
+    printf("--- %s\n", lua_tostring(L, -1));
+    lua_pop(L, 1); // remove error message from Lua's stack
+    exit(EXIT_FAILURE);
+  }
+  
   std::string rescaling(std::string _phrase, std::pair<float,float> _range) {
     auto parseRange = parserCode + " range_min =\"" + std::to_string(_range.first) +  "\" ;range_max=\"" + std::to_string(_range.second) +
      "\" ;rs = table.concat(lpeg.match(rangeG,\"" + _phrase + "\"),\" \")";
     
-    if (luaL_dostring(L, parseRange.c_str()) == LUA_OK) {
+    if (int luaError = luaL_dostring(L, parseRange.c_str()) == LUA_OK) {
       lua_getglobal(L,"rs");
       
       if (lua_isstring(L,-1))
         _phrase = lua_tostring(L,-1);
-    }
+    } else
+        reportErrors(L, luaError);
+    
     return _phrase;
   }
 
@@ -150,7 +157,7 @@ public:
 
     auto p = parserCode + " t = lpeg.match(phraseG, \"" + _phrase + "\")";
 
-    if (luaL_dostring(L, p.c_str()) == LUA_OK) {
+    if (int luaError = luaL_dostring(L, p.c_str()) == LUA_OK) {
       lua_getglobal(L, "t");
       
       // 3D Lua table to c++ vector
@@ -228,7 +235,9 @@ public:
           lua_pop(L,2);
         }    
       }
-    }
+    } else
+        reportErrors(L, luaError);
+    
     return v;
   }
 } parser;
